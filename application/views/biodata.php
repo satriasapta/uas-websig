7797c207c43b7d8d880e1ec39eedba91da6a9b88
User's <Melan120322>
2022-12-26 21:54:33 +0700

    COMMIT MELAN CANIADI

---
+++ application/views/biodata.php
@@ -0,0 +1,31 @@
+<div class="content-padder content-background">
+    <div class="uk-section-small uk-section-default header">
+        <div class="uk-container uk-container-large">
+            <h2><span uk-icon="icon: file-text; ratio: 2"></span> <?= $judul ?></h2>
+        </div>
+    </div>
+    <div class="uk-section-small">
+        <div class="uk-container uk-container-large">
+            <article class="uk-comment uk-comment-primary">
+                <header class="uk-comment-header">
+                    <div class="uk-grid-medium uk-flex-middle" uk-grid>
+                        <div class="uk-width-auto">
+                            <img class="uk-comment-avatar" src="<?= base_url('public/') ?>images/logo.png" width="80" height="80" alt="">
+                        </div>
+                        <div class="uk-width-expand">
+                            <h4 class="uk-comment-title uk-margin-remove"><a class="uk-link-reset" href="#">Dibuat Oleh :</a></h4>
+                            <h4 class="uk-comment-title uk-margin-remove"><a class="uk-link-reset" href="#">Satria, Aura, Melan</a></h4>
+
+                        </div>
+                    </div>
+                </header>
+                <div class="uk-comment-body">
+                    <b>
+                        <center>Aplikasi Sistem Transportasi Darat</center>
+                    </b>
+                </div>
+            </article>
+
+
+        </div>
+    </div>
