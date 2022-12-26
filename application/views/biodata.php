bfd897a8b59e611d1d9b93f510687bd9814255c6
User's <Melan120322>
2022-12-26 21:56:55 +0700

    COMMIT MELAN CANIADI

--- application/views/biodata.php
+++ application/views/biodata.php
@@ -21,7 +21,7 @@
                 </header>
                 <div class="uk-comment-body">
                     <b>
-                        <center>Aplikasi Sistem Transportasi Darat</center>
+                        <center>APLIKASI SISTEM TRANSPORTASI DARAT </center>
                     </b>
                 </div>
             </article>
